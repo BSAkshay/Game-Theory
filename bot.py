5c53748a5a9c5ba89ff3005da4052c529171647d
BSAkshay <akshaysinghbchehhat@gmail.com>
2026-06-21 22:56:01 +0530

    Remove bot_decision function from bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,1 @@
-import random
 
-def bot_decision(state):
-    belief = state["players"][1]["belief_variance"]
-    if belief < 0.4:
-        return random.choice(["A", "B", "C"])
-    return random.choice(["A", "B", "C"])
